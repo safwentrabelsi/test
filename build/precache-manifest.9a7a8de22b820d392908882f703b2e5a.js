8cc514a10b56c8ed653e19e17cb131c9c5938c7f
safwentrabelsi <safwentrabelsi95@gmail.com>
2018-10-05 04:11:46 +0100

    first commit

---
+++ build/precache-manifest.9a7a8de22b820d392908882f703b2e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bbe89cf61c4d35ddd736ef832fcbdb2",
+    "url": "/static/media/LogoMedgoApp.9bbe89cf.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b1668e7ccbfafaef5f9",
+    "url": "/static/js/main.8b1668e7.chunk.js"
+  },
+  {
+    "revision": "b97294f8547e99b92d5c",
+    "url": "/static/js/1.b97294f8.chunk.js"
+  },
+  {
+    "revision": "8b1668e7ccbfafaef5f9",
+    "url": "/static/css/main.f61a4976.chunk.css"
+  },
+  {
+    "revision": "236b007219f98832a6f85835f17c8c38",
+    "url": "/index.html"
+  }
+];
